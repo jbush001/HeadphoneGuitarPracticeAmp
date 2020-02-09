d5aa695333d5b86df9af3fedb75cef7b7991abc3
Jeff Bush <jeffbush001@gmail.com>
2020-02-09 14:59:42 -0800

    Basic blinky test app.

--- firmware/main.c
+++ firmware/main.c
@@ -1,5 +1,21 @@
 
+//
+// Blink the LED attached to PB11
+//
+
+volatile unsigned int * const REGB_BASE = ((unsigned int*) 0x48000400);
+
+#define MODE_REG 0
+#define OD_REG 5
+
+
 void main() {
-    while (1)
-        ;
+    unsigned int counter = 0;
+
+    REGB_BASE[MODE_REG] = 1 << 22;  // Make this port an output, everything else an input
+
+    while (1) {
+        REGB_BASE[OD_REG] = (((counter >> 25) & 1) << 11);
+        counter++;
+    }
 }
