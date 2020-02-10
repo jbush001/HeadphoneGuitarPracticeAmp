43511e8f6407998fc5e0a575fe7044cf54632be5
Jeff Bush <jeffbush001@gmail.com>
2020-02-09 18:07:17 -0800

    Enable clocks to GPIO block in blinking program.
    I won't be able to test this until I get the boards back and assemble them, so this
    is speculative based on datasheets for now.

--- firmware/main.c
+++ firmware/main.c
@@ -3,19 +3,23 @@
 // Blink the LED attached to PB11
 //
 
-volatile unsigned int * const REGB_BASE = ((unsigned int*) 0x48000400);
-
-#define MODE_REG 0
-#define OD_REG 5
-
+#define GPIO_REGB_BASE ((volatile unsigned int*) 0x48000400)
+#define RCC_BASE ((volatile unsigned int*) 0X40021000)
+#define GPIO_MODE 0
+#define GPIO_OD 5
+#define RCC_AHB2EN (0x4c / 4)
 
 void main() {
     unsigned int counter = 0;
 
-    REGB_BASE[MODE_REG] = 1 << 22;  // Make this port an output, everything else an input
+    // Enable clocks for GPIO blocks A, B, and C
+    RCC_BASE[RCC_AHB2EN] = 0x7;
+
+    // Make PB11 an output, everything else an input
+    GPIO_REGB_BASE[GPIO_MODE] = 1 << 22;
 
     while (1) {
-        REGB_BASE[OD_REG] = (((counter >> 25) & 1) << 11);
+        GPIO_REGB_BASE[GPIO_OD] = (((counter >> 25) & 1) << 11);
         counter++;
     }
 }
