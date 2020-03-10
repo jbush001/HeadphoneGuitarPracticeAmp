6892f2c86de0afde27e1453f8647efd4d1216ce4
Jeff Bush <jeffbush001@gmail.com>
2020-03-09 20:06:32 -0700

    Fix flash command

--- firmware/flashit.sh
+++ firmware/flashit.sh
@@ -1,3 +1,3 @@
 arm-none-eabi-gcc -O3 -mcpu=cortex-m4 -Wl,--script link.ld -nostdlib start.S main.c -o firmware.elf
 arm-none-eabi-objcopy -O binary firmware.elf firmware.bin
-#st-flash firmware.bin
+st-flash write firmware.bin 0x8000000
